b151e1f016d1209da9e1b4be9b34f655860e8f17
Ron Erick Rodriguez <c_rdrodriguez@unilab.com.ph>
2023-08-16 16:30:05 +0800

    Initial Commit

---
+++ lib/screens/onboding/components/buttons/animated_btn.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:rive/rive.dart';
+import 'package:flutter/material.dart';
+
+class AnimatedBtn extends StatelessWidget {
+  const AnimatedBtn({
+    super.key,
+    required this.press,
+  });
+
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ),
+        ),
+        child: const SizedBox(
+          height: 65,
+          width: 260,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                top: 0,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      CupertinoIcons.forward,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width: 9),
+                    Text(
+                      "Click to Start",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
